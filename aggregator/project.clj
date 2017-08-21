3e3d58e56e0af0ac28cd9d4584e1984f7a3fecde
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-21 14:35:30 +0200

    Update dependencies, add plugins

--- aggregator/project.clj
+++ aggregator/project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
+             :uberjar {:aot :all}}
+
+  :plugins [[lein-kibit "0.1.5"]
+            [lein-ancient "0.6.10"]]
+
   :main ^:skip-aot aggregator.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
