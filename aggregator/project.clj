540aa46743775a9692269b07c3625b8585ff2ddc
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-21 14:54:16 +0200

    Add dependency for langohr

--- aggregator/project.clj
+++ aggregator/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [com.novemberain/langohr "3.6.1"]]
 
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
              :uberjar {:aot :all}}
--- aggregator/test/aggregator/core_test.clj
+++ aggregator/test/aggregator/core_test.clj
@@ -3,5 +3,5 @@
             [aggregator.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "At least we have one test!"
+    (is true)))
