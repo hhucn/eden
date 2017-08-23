3bddf234d052232f4db2ff29a2058265b75621b0
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-23 15:36:20 +0200

    Add initial db schema

---
+++ db/entrypoint/001_initial.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA IF NOT EXISTS aggregator
+AUTHORIZATION aggregator;
+
+CREATE TABLE IF NOT EXISTS aggregator.events (
+       sequence_number SERIAL PRIMARY KEY,
+       version smallint NOT NULL,
+       aggregate_id text,
+       creator text  NOT NULL,
+       created  TIMESTAMP NOT NULL DEFAULT now(),
+       type text  NOT NULL,
+       data text
+)
+WITH (
+     OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE INDEX on aggregator.events (aggregate_id);
+CREATE INDEX on aggregator.events (creator);
+CREATE INDEX on aggregator.events (type);
+
+
+ALTER TABLE aggregator.events OWNER to aggregator;
