604ac82fc95f36f919ab2e5c20b5da5ff2a94ff1
Alexander Schneider <alexander@schneider.gg>
2017-08-23 10:56:49 +0200

    Implement a preliminary version for get-statement in query module

--- aggregator/src/aggregator/query/query.clj
+++ aggregator/src/aggregator/query/query.clj
@@ -1,9 +1,13 @@
-(ns aggregator.query.query) ;;TODO
+(ns aggregator.query.query
+  (:require [aggregator.query.cache :as cache]))
 
 ;; The main module for queries. All internal calls run through the functions defined here. The other aggregator module call these functions, as well as the utility-handlers.
 
-(defn statement [id]
-  id)
+(defn statement [uri]
+  (let [result (cache/retrieve uri)]
+    (if (= result :missing)
+      (from-db uri)
+      result)))
 
 (defn discussion [id]
   id)
