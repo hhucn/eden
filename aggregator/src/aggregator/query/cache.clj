b18a6ee4fa0860ae78ce3c1c1dbefe18171510fe
Alexander Schneider <alexander@schneider.gg>
2017-08-23 10:49:15 +0200

    Touch up on the cache code

--- aggregator/src/aggregator/query/cache.clj
+++ aggregator/src/aggregator/query/cache.clj
@@ -5,28 +5,27 @@
 
 ;; This module handles the dynamic caching to improve the performance of the queries.
 
+(def storage (atom (cache/lru-cache-factory {} :threshold 2000)))
+
+(defn retrieve
+  "Try to retrieve an item from cache and trigger the appropriate events.
+  Should always be followed by a filling of the value if possible."
+  [uri]
+                                        ;TODO
+  )
+
 (defn cache-hit
   "Touch the item in the cache and retrieve it."
   [uri]
   (swap! storage #(cache/hit % uri))
   (get @storage uri))
 
-(defn cache-miss
-  "Retrieve the item on the long way if possible and then add it to the cache before delivering it."
+(defn cache-miss ;;TODO
+  "Signalize that an item was missing and fill in the missing value."
   [uri]
   (let [statement (query/query uri)]
     (swap! storage #(cache/miss % uri statement)))
   (get @storage uri))
-
-(defn get-statement
-  "Retrieve statements (currently only of the dbas:: domain)"
-  [uri]
-  (if (cache/has? @storage uri)
-    (cache-hit uri)
-    (cache-miss uri)))
-
-                                        ;(get-statement "dbas::2")
-                                        ;(get-statement "spon.de::35")
 
 (defn get-cached-statements
   "Retrieve all arguments currently in the cache"
--- aggregator/src/aggregator/query/query.clj
+++ aggregator/src/aggregator/query/query.clj
@@ -1,4 +1,16 @@
-(ns ) ;;TODO
+(ns aggregator.query.query) ;;TODO
 
 ;; The main module for queries. All internal calls run through the functions defined here. The other aggregator module call these functions, as well as the utility-handlers.
+
+(defn statement [id]
+  id)
+
+(defn discussion [id]
+  id)
+
+(defn statement-tree [id]
+  [id id id])
+
+(defn add-statement [statement]
+  {:status :okay})
 
