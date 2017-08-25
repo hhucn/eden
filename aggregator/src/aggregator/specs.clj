242acc3b802a2988efd597b3aa679745a8b6fa36
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-25 17:26:45 +0200

    Prepare spec for statements

--- aggregator/src/aggregator/specs.clj
+++ aggregator/src/aggregator/specs.clj
@@ -6,7 +6,7 @@
 (s/def ::creator string?)
 (s/def ::type string?)
 (s/def ::aggregate_id string?)
-(s/def ::entity_id string?)
+(s/def ::entity_id uuid?)
 (s/def ::data string?)
 
 (s/def ::event
@@ -15,3 +15,12 @@
 (comment
   (s/exercise ::event)
   )
+
+(s/def ::data map?)
+
+(s/def ::statement
+  (s/keys :req-un [::aggregate_id ::data]
+          :opt-un [::entity_id]))
+
+(s/exercise ::statement)
+
