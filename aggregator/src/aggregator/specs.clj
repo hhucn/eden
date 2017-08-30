90a2ede005b81c00156457a793eec78705b2f769
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-30 14:32:01 +0200

    Update specs, define data-structures

--- aggregator/src/aggregator/specs.clj
+++ aggregator/src/aggregator/specs.clj
@@ -1,26 +1,36 @@
 (ns aggregator.specs
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::sequence_number pos-int?)
+(s/def ::no-slash (s/and string? #(not (re-find #"/" %)) #(pos? (count %))))
+
+(s/def ::author string?)
+(s/def ::content string?)
+(s/def ::aggregate-id ::no-slash)
+(s/def ::entity-id ::no-slash)
 (s/def ::version pos-int?)
-(s/def ::creator string?)
-(s/def ::type string?)
-(s/def ::aggregate_id string?)
-(s/def ::entity_id uuid?)
-(s/def ::data string?)
-
-(s/def ::event
-  (s/keys :req-un [::sequence_number ::version ::creator ::type]
-          :opt-un [::aggregate_id ::entity_id ::data]))
-(comment
-  (s/exercise ::event)
-  )
-
-(s/def ::data map?)
-
+(s/def ::created any?) ;; timestamp
+(s/def ::ancestor-aggregate-id ::no-slash)
+(s/def ::ancestor-entity-id ::no-slash)
+(s/def ::ancestor-version ::version)
 (s/def ::statement
-  (s/keys :req-un [::aggregate_id ::data]
-          :opt-un [::entity_id]))
+  (s/keys :req-un [::author ::content
+                   ::aggregate-id ::entity-id ::version
+                   ::created]
+          :opt-un [::ancestor-aggregate-id ::ancestor-entity-id ::ancestor-version]))
+;; (s/exercise ::statement)
 
-(s/exercise ::statement)
 
+(s/def ::from-aggregate-id ::no-slash)
+(s/def ::from-entity-id ::no-slash)
+(s/def ::from-version ::version)
+(s/def ::to-aggregate-id ::no-slash)
+(s/def ::to-entity-id ::no-slash)
+(s/def ::to-version ::version)
+(s/def ::link
+  (s/keys :req-un [::author ::type
+                   ::from-aggregate-id ::from-entity-id ::from-version
+                   ::to-aggregate-id ::to-entity-id
+                   ::aggregate-id ::entity-id
+                   ::created]
+          :opt-un [::to-version]))
+;; (s/exercise ::link)
