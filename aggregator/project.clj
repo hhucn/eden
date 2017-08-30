3f79f8939cd206eb7cfb82ec65de36a99b20f434
Alexander Schneider <alexander@schneider.gg>
2017-08-30 09:27:52 +0200

    Remove doubled dependency and sort them so this does not happen again

--- aggregator/project.clj
+++ aggregator/project.clj
@@ -5,16 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
                  [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.cache "0.6.5"]
+                 [org.postgresql/postgresql "42.1.4"]
                  [com.novemberain/langohr "4.1.0"]
                  [korma "0.4.3"]
-                 [org.postgresql/postgresql "42.1.4"]
                  [compojure "1.6.0"]
                  [ring/ring-defaults "0.3.1"]
                  [ring/ring-json "0.4.0"]
                  [ring/ring-mock "0.3.1"]
                  [ring/ring-jetty-adapter "1.6.2"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/core.cache "0.6.5"]
                  [clj-http "3.7.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
              :repl {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]]}
