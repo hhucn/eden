b16c80061056d713b5c32e640ee90411aa71466a
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-08-31 16:48:38 +0200

    Remove exchange creation, create statement-specific queues

--- aggregator/src/aggregator/broker/connector.clj
+++ aggregator/src/aggregator/broker/connector.clj
@@ -3,11 +3,10 @@
             [langohr.core :as rmq]
             [langohr.channel :as lch]
             [langohr.exchange :as le]
-            [langohr.queue :as lq]))
+            [langohr.queue :as lq]
+            [aggregator.broker.lib :as blib]))
 
-(def exchange "argweb")
-(def queues
-  {:statement "statement.update"})
+(def exchange "argweb") ;; Currently unused
 
 ;; -----------------------------------------------------------------------------
 ;; Setup broker
@@ -28,22 +27,31 @@
   (le/declare channel exchange-name "topic" {:durable true :auto-delete false :exclusive false})
   channel)
 
-(defn- create-queue [channel queue-name]
-  (lq/declare channel queue-name {:durable true :auto-delete false :exclusive false})
+(defn create-queue
+  "Creates a queue for a given entity. Extracts the original aggregator and the
+  entity id from the provided entity."
+  [channel entity]
+  (let [queue-name (blib/get-queue-name entity)]
+    (lq/declare channel queue-name {:durable true :auto-delete false :exclusive false})
+    (lq/bind channel queue-name exchange))
   channel)
 
-(defn open-channel []
+(defn open-channel
+  "Opens a channel for an existing connection to the broker."
+  []
   (lch/open @conn))
-(defn close-channel [ch]
+
+(defn close-channel
+  "Given a channel, close it!"
+  [ch]
   (lch/close ch))
 
-(defn init-connection! []
+(defn init-connection!
+  "Initializes connection to broker and creates an exchange."
+  []
   (create-connection!)
   (log/debug "Connection to Message Broker established.")
   (let [ch (open-channel)]
     (-> ch
-        (create-exchange exchange)
-        (create-queue (:statement queues)))
-    (lq/bind ch (:statement queues) exchange)
-    (lq/bind ch (:issue queues) exchange)
-    (close-channel ch)))
+        #_(create-exchange exchange)
+        (close-channel))))
