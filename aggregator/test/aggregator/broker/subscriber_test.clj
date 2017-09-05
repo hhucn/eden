6385523f03e82d5238a84a1dacf43a79781b3165
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-09-05 10:38:55 +0200

    Make subscriber-handler more generic; add test for it

--- aggregator/test/aggregator/broker/subscriber_test.clj
+++ aggregator/test/aggregator/broker/subscriber_test.clj
@@ -1,4 +1,31 @@
 (ns aggregator.broker.subscriber-test
-  (:require [aggregator.broker.subscriber :as sut]
-            [clojure.test :refer [deftest is are]]))
+  (:require [clojure.test :refer [deftest is are use-fixtures]]
+            [aggregator.broker.subscriber :as sub]
+            [aggregator.broker.publish :as pub]
+            [aggregator.broker.connector :as connector]
+            [aggregator.utils.common :as lib]))
 
+(def queue (str (lib/uuid)))
+
+(def statement {:author "kangaroo"
+                :content "Schnapspralinen"
+                :aggregate-id "huepfer.verlag"
+                :entity-id "1"
+                :version 1
+                :created nil})
+
+;; Test preparation
+(defn fixtures [f]
+  (connector/init-connection!)
+  (connector/create-queue queue)
+  (pub/publish-statement statement)
+  (f)
+  (connector/close-connection!))
+(use-fixtures :once fixtures)
+
+
+;; -----------------------------------------------------------------------------
+;; Tests
+
+(deftest subscribe
+  (is true (sub/subscribe queue (fn [payload] (println "Received some content: " payload)))))
