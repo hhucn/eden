0a9aa61c469c9ed6684a1ed81098f755a29e73a8
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-09-08 11:08:25 +0200

    Update clojure

--- aggregator/project.clj
+++ aggregator/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.cache "0.6.5"]
                  [org.clojure/core.async "0.3.443"]
