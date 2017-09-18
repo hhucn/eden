6de2df95e3ed9a181b89c5e0683d0510dad6009a
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-09-18 12:28:12 +0200

    Remove doubled namespace

--- aggregator/test/aggregator/search/core_test.clj
+++ aggregator/test/aggregator/search/core_test.clj
@@ -3,9 +3,10 @@
             [aggregator.search.core :as search]))
 
 (deftest create-delete-index
-  (testing "Index can be created once, but not twice."
+  (testing "Only one index of the same kind is allowed."
     (let [some-keyword :kangaroo]
       (are [x y] (= x y)
         :ok (:status (search/create-index some-keyword))
         :error (:status (search/create-index some-keyword))
-        :ok (:status (search/delete-index some-keyword))))))
+        :ok (:status (search/delete-index some-keyword))
+        :error (:status (search/delete-index some-keyword))))))
