52b6a713fe8c52e1962d84cb35be1098f536d368
Alexander Schneider <alexander@schneider.gg>
2017-09-20 16:23:25 +0200

    Add logge rline

--- aggregator/src/aggregator/core.clj
+++ aggregator/src/aggregator/core.clj
@@ -1,9 +1,11 @@
 (ns aggregator.core
-  (:require [aggregator.query.retriever :as retriever])
+  (:require [aggregator.query.retriever :as retriever]
+            [taoensso.timbre :as log])
   (:gen-class))
 
 (defn -main
   "Bootstrap everything needed for the provider."
   [& args]
   (retriever/bootstrap)
-  (println "Started all Services!"))
+  (println "Started all Services!")
+  (log/debug "Main Bootstrap finished"))
