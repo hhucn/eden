334580965c8512d7ef772e48a30ac47d137fdfea
Alexander Schneider <alexander@schneider.gg>
2017-10-17 16:29:11 +0200

    Add tests for query module

--- aggregator/test/aggregator/query/query_test.clj
+++ aggregator/test/aggregator/query/query_test.clj
@@ -3,3 +3,10 @@
             [clojure.test :refer [deftest is]]))
 
 ;; Here be tests
+(deftest test-retrieve-link
+  (is (not= :not-found (query/retrieve-link "schneider.gg/link0r1337")))
+  (is (= :not-found (query/retrieve-link "foo.bar/nonexistent"))))
+
+(deftest local-tiered-retrieval
+  (is (not= :not-found (query/tiered-retrieval "hhu.de/34" {:opts [:no-remote]})))
+  (is (= :not-found (query/tiered-retrieval "foo.bar/nonexistent" {:opts [:no-remote]}))))
