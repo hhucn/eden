2a57e50af286c5b87445b403c80249364e2018ef
Alexander Schneider <alexander@schneider.gg>
2017-10-18 16:34:12 +0200

    Add listener for D-BAS DB changes. Unfinished

---
+++ aggregator/src/aggregator/utils/pg_listener.clj
@@ -0,0 +1,41 @@
+(ns aggregator.utils.pg-listener
+  (:require [postgres-listener.core :as pgl]
+            [aggregator.config :as config]
+            [taoensso.timbre :as log]))
+
+
+(defn- handle-statements
+  "Handle changes in statements"
+  [statement]
+  (log/debug (str "new statement: " statement)))
+
+(defn- handle-textversions
+  "Handle changes in the textversions. They belong to the statements."
+  [textversion]
+  (log/debug "new textversion: " (:data textversion))
+  {:author (get-in textversion [:data :author_uid])
+   :content (get-in textversion [:data :content])
+   :aggregate-id config/aggregate-name
+   :entity-id (get-in textversion [:data :uid])
+   :version 1
+   :created (get-in textversion [:data :timestamp])})
+
+(defn- handle-arguments
+  "Handle changes in arguments and update links correspondingly."
+  [argument])
+
+(defn start-listeners
+  "Start all important listeners."
+  []
+  (pgl/connect {:host "dbas-db"
+                :port (read-string (System/getenv "DBAS_DB_PORT"))
+                :database "discussion"
+                :user (System/getenv "DBAS_DB_USER")
+                :password (System/getenv "DBAS_DB_PW")})
+  (pgl/arm-listener handle-statements "statements_changes")
+  (pgl/arm-listener handle-textversions "textversions_changes")
+  (pgl/arm-listener handle-arguments "arguments_changes")
+  (log/debug "Started listeners for DBAS-PG-DB"))
+
+(start-listeners)
+
