2073de61e373c44b4bbb7dc004118a9cd1af7e9f
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-12-14 10:33:47 +0100

    Add start script
    Add start script
    
    Fix start-script
    
    Use function call for hostname

---
+++ start_services.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ":: Starting first set..."
+docker-compose up -d
+
+echo ":: Starting second set..."
+docker-compose -f docker-compose.set2.yml up -d
+
+echo ":: Starting portainer..."
+docker run --rm -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
+
+echo ":: Check the status at http://$(hostname):9000"
