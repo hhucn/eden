605530d8e453fc5b6cda16593f677376191e7967
Alexander Schneider <alexander@schneider.gg>
2017-12-15 14:47:11 +0100

    Change aggregator-name to be a specific environment variable

--- aggregator/src/aggregator/config.clj
+++ aggregator/src/aggregator/config.clj
@@ -4,7 +4,7 @@
   #{"aggregator:8888" "aggregator_set2:8888"})
 
 (def aggregate-name
-  "aggregator:8888")
+  (System/getenv "AGGREGATOR-NAME"))
 
 
 ;; -----------------------------------------------------------------------------
