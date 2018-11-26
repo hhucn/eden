dc99b49b7850c2e687fb7036cbf71f8a172b97f9
Alexander Schneider <alexander@schneider.gg>
2018-11-26 12:49:13 +0100

    Fix test

--- aggregator/test/aggregator/api/routes_test.clj
+++ aggregator/test/aggregator/api/routes_test.clj
@@ -16,9 +16,9 @@
 
 
 (deftest handler-test
-  (testing "Test root route for status 200"
+  (testing "Test root route for status 302"
     (let [response (routes/app (mock/request :get "/"))]
-      (is (= (:status response) 200)))))
+      (is (= (:status response) 302)))))
 
 (deftest statement-nonexistent-route
   (testing "Test nonexistent-route for status 500"
